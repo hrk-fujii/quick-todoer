e84f7d0467f3570a1b64643a2b792a05e1321797
Hiroki Fujii <hfujii@hisystron.com>
2022-03-09 16:26:08 +0900

    TODOビューの通信を実装

--- components/todoViewer/TodoViewer.tsx
+++ components/todoViewer/TodoViewer.tsx
@@ -1,13 +1,35 @@
 import React from "react";
+import * as firebaseAuth from "firebase/auth";
+import * as fireStore from "firebase/firestore";
 import {task} from "../../defines/types";
 import {ScrollView} from "native-base";
 import Todo from "./Todo";
 
 const TodoViewer = () => {
+    const [schedules, setSchedules] = React.useState<object>({});
+    
+    const user = firebaseAuth.getAuth().currentUser;
+    const db = fireStore.getFirestore();
+
+    const getSchedules = async () => {
+        const schedulesResult = await fireStore.getDocs(fireStore.collection(db, "users/" + user?.uid + "/schedules"));
+        let returnData = {};
+        schedulesResult.forEach(element => {
+            returnData[element.id] = element.data;
+        });
+        return returnData;
+    }
+    
+    React.useEffect(() => {
+        getSchedules().then((data) => {
+            setSchedules(data);
+        });
+    }, []);
+    
     let todoList: React.FC[] = [];
 
-    for (const todoData of sampleList) {
-        todoList.push(<Todo data={todoData} />);
+    for (const [key, val] of Object.entries(schedules)) {
+        todoList.push(<Todo data={val} id={key} />);
     }
 
     
