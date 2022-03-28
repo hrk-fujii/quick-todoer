0d15d6f2a02573a2f13a2367c71d4f5a0abde39d
Hiroki Fujii <hfujii@hisystron.com>
2022-03-28 23:38:26 +0900

    ID/Passログインに仕様変更 - ログイン画面を実装

--- components/authentication/Login.tsx
+++ components/authentication/Login.tsx
@@ -1,39 +1,48 @@
 import React from 'react';
-import * as expoWebBrowser from 'expo-web-browser';
-import { ResponseType } from 'expo-auth-session';
-import * as googleExpoAuthSessionProvider from 'expo-auth-session/providers/google';
 import * as firebaseAuth from 'firebase/auth';
-import { Row, Center, Text, Button } from "native-base";
+import { Row, Center, Text, Input, Button } from "native-base";
 import { googleExpoAuthSessionProviderConfig } from '../.././config';
 
 
 const Login = () => {
+  const auth = firebaseAuth.getAuth();
+  
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  const [errorMessage, setErrorMessage] = React.useState<string>("");
 
-  const [request, response, promptAsync] = googleExpoAuthSessionProvider.useIdTokenAuthRequest(googleExpoAuthSessionProviderConfig,);
-
-  React.useEffect(() => {
-    if (response?.type === 'success') {
-      const { id_token } = response.params;
-      const auth = firebaseAuth.getAuth();
-      // const provider = new GoogleAuthProvider();
-      const credential = firebaseAuth.GoogleAuthProvider.credential(id_token);
-      firebaseAuth.signInWithCredential(auth, credential);
+  const hSubmit = async () => {
+    try {
+      await firebaseAuth.signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      const messageMatch = error.toString().match(/\(auth\/(.*)\)/);
+      let message = "";
+      if (message !== null) {
+        message = messageMatch[1];
+      }
+      setErrorMessage(message);
     }
-  }, [response]);
+  }
 
-  return (
-    <Center flex={1}>
-      <Text>ようこそ!</Text>
-      <Text>開始するには、サインインしてください。</Text>
-      <Button
-        disabled={!request} onPress={() => {
-          promptAsync();
-        }}
-      >
-        <Text>Googleでサインイン</Text>
+  return <Center flex={1}>
+    <Row>
+      <Text>ログイン</Text>
+    </Row>
+    <Row>
+      <Text>メールアドレス</Text>
+      <Input type='email' keyboardType='email-address' onChangeText={(t) => {setEmail(t)}} />
+    </Row>
+    <Row>
+      <Text>パスワード</Text>
+      <Input type="password" onChangeText={(t) => {setEmail(t)}} />
+    </Row>
+    <Row>
+      <Text>{errorMessage}</Text>
+      <Button onPress={hSubmit}>
+        ログイン
       </Button>
-      </Center>
-  );
+    </Row>
+  </Center>
 }
 
 export default Login;
