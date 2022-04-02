ef5d0431274159ae7c7738937f12c9687650dd26
Hiroki Fujii <hfujii@hisystron.com>
2022-04-02 15:12:46 +0900

    ログイン画面のちらつきを修正

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {SSRProvider} from "@react-aria/ssr";
 import {RecoilRoot} from "recoil";
 import { extendTheme, NativeBaseProvider, Box } from "native-base";
-import * as expoWebBrowser from 'expo-web-browser';
 import * as firebaseApp from "firebase/app";
 import * as firebaseAuth from "firebase/auth";
 import {firebaseConfig} from "./config";
 import Signin from "./components/authentication/Login";
 import Signup from "./components/authentication/Signup";
+import Loading from "./components/container/Loading";
 import MainContainer from "./components/container/Main";
 
 // Initialize Firebase
@@ -15,23 +15,45 @@ firebaseApp.initializeApp(firebaseConfig);
 
 expoWebBrowser.maybeCompleteAuthSession();
 
+
+const customComponentTheme = {
+  Button: {
+    defaultProps: {
+      _dark: {
+        bg: "rgb(0, 0, 200)",
+        color: "rgb(255,255,255)"
+      },
+      _light: {
+        bg: "rgb(150, 150, 255)",
+        color: "rgb(0,0,0)"
+      }
+    }
+  }
+}
+
 const customThemeConfig = {
   useSystemColorMode: true,
   initialColorMode: "dark"
 };
-const customTheme = extendTheme({config: customThemeConfig});
+const customTheme = extendTheme({
+  components: customComponentTheme,
+  config: customThemeConfig
+});
 
 
 function App() {
   const [authorized, setAuthorized] = React.useState<boolean>(false);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
   const [signup, setSignup] = React.useState<boolean>(false);
   
   const auth = firebaseAuth.getAuth();
   firebaseAuth.onAuthStateChanged(auth, (user) => {
     if (user) {
       setAuthorized(true);
+      setIsLoading(false);
     } else {
       setAuthorized(false);
+      setIsLoading(false);
     }
   });
 
@@ -39,7 +61,7 @@ function App() {
     <NativeBaseProvider theme={customTheme}>
       <RecoilRoot>
         <Box flex={1} _dark={{bg: "black"}}>
-          {authorized ? <MainContainer /> : ( signup ? <Signup setSignup={()=>{setSignup(false)}}/> : <Signin setSignup={()=>{setSignup(true)}}/> )}
+          {authorized ? ( isLoading ? <Loading /> : <MainContainer /> ) : ( signup ? <Signup setSignup={()=>{setSignup(false)}}/> : <Signin setSignup={()=>{setSignup(true)}}/> )}
         </Box>
       </RecoilRoot>
     </NativeBaseProvider>
